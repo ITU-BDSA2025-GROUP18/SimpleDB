2eb53ee09efb7d014203df5e68a4538851fa39fe
MillrDev <millervinjr@proton.me>
2025-09-05 11:52:39 +0200

    Created CSBDatabase class and IDatabaseRepository interface

---
+++ CSVDatabase.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace SimpleDB;
+
+public sealed class CSVDatabase<T> : IDatabaseRepository<T>
+{
+    public IEnumerable<T> Read(int? limit = null)
+    {
+        return null;
+    }
+
+    public void Store(T record)
+    {
+    }
+}
