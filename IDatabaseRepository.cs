1019380b78cab8a0ced832041d5a9914dec6d564
erze <228920027+EranZejnelovski@users.noreply.github.com>
2025-09-05 15:32:35 +0200

    Interface created

---
+++ IDatabaseRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleDB;
+
+public interface IDatabaseRepository<T>
+{
+    //Function that gets all cheeps from the crip_cli_db.csv file
+    public IEnumerable<T> Read(int limit); //For at gøre den generic (Fremgået også på billedet
+    
+    //Tilføjer en besked til chirp_cli_db.csv, inkl. user og tidspunkt
+    public void Store(T message); //For at gøre den generic (Fremgår også på billedet)
+}   
